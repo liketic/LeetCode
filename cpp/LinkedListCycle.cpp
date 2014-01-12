c380371b0a36cc4bcd81aba4730715dc210ea8f2
Li Ke <like_0903@163.com>
2014-01-12 15:57:14 +0800

    5th

---
+++ cpp/LinkedListCycle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ListNode *p = head, *q = head;
+        
+        while (p && q) {
+            p = p->next;
+            q = q->next;
+            if (q == NULL)
+                return false;
+            q = q->next;
+            if (p && q && (p == q))
+                return true;
+        }
+        return false;
+        
+    }
+};
