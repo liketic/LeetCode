20587b878715cb43927b06e47cd64935e629e4c5
likecpp <likegit@gmail.com>
2014-06-22 17:43:41 +0800

    remove

--- cpp/SubsetsII.cpp
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public:
-        vector<vector<int> > subsetsWithDup(vector<int> &num) {
-         
-        }
-};
--- java/SingleNumber.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public int singleNumber(int[] A) {
-        int ans = 0;
-        for (int i = 0; i < A.length; i++)
-            ans ^= A[i];
-        return ans;
-    }
-}
