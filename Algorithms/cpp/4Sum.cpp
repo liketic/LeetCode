dd633886dd4520876cc81bfcc02b1e048487be46
Li Ke <ke.like@outlook.com>
2016-03-27 10:21:34 +0800

    Refactor and rename some cpps

--- Algorithms/cpp/4Sum.cpp
+++ Algorithms/cpp/4Sum.cpp
@@ -1,13 +1,36 @@
+// Source : https://leetcode.com/problems/4sum/
+// Author : Ke Li
+// Date   : 2016-03-27
+
+/*************************************************************************************** 
+ *
+ * Given an array S of n integers, are there elements a, b, c, and d in S such that a + 
+ * b + c + d = target? Find all unique quadruplets in the array which gives the sum of 
+ * target.
+ * 
+ * Note:
+ * 
+ * Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ 
+ * d)
+ * The solution set must not contain duplicate quadruplets.
+ * 
+ *     For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+ * 
+ *     A solution set is:
+ *     (-1,  0, 0, 1)
+ *     (-2, -1, 1, 2)
+ *     (-2,  0, 0, 2)
+ ***************************************************************************************/
 
 const int MAX_SIZE = 1000005;
-struct node {
-    int v;
-    int x, y;
+struct Node {
+    int v, x, y;
 };
 
-node d[MAX_SIZE];
+Node d[MAX_SIZE];
 
-bool cmp(const node& a, const node& b) {
+// custom Node Comparator
+bool comparator(const Node& a, const Node& b) {
     if (a.v != b.v)
         return a.v < b.v;
     if (a.x != b.x)
@@ -18,29 +41,36 @@ bool cmp(const node& a, const node& b) {
 class Solution {
 public:
     vector<vector<int> > fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int k = 0;
+        
+        int pos = 0;
+        int n = nums.size();
+        
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                d[k].v = nums[i] + nums[j];
-                d[k].x = i;
-                d[k++].y = j;
+                d[pos].v = nums[i] + nums[j];
+                d[pos].x = i;
+                d[pos++].y = j;
             }
         }
-        sort(d, d + k, cmp);
-        vector<vector<int> > ans;
-        for (int i = 0; i < k; i++) {
+        
+        // sort the combine sum array
+        sort(d, d + pos, comparator);
+        
+        vector<vector<int> > matchlist;
+        
+        for (int i = 0; i < pos; i++) {
             int temp = target - d[i].v;
-            int l = i + 1, r = k - 1;
-
+            int l = i + 1, r = pos - 1;
+            
             while (l <= r) {
                 int m = (l + r) / 2;
                 if (d[m].v == temp) {
                     int j = m;
+                    // get the index before first
                     while (j >= 0 && d[j].v == temp)j--;
-                    j++;
-                    for (; j < k; j++) {
+                    
+                    for (j++; j < pos; j++) {
                         if (d[j].v != temp) break;
                         if (d[j].x != d[i].x && d[j].x != d[i].y
                                 && d[j].y != d[i].x && d[j].y != d[i].y) {
@@ -50,7 +80,7 @@ public:
                             item.push_back(nums[d[j].x]);
                             item.push_back(nums[d[j].y]);
                             sort(item.begin(), item.end());
-                            ans.push_back(item);
+                            matchlist.push_back(item);
                         }
                     }
                     break;
@@ -61,24 +91,32 @@ public:
                 }
             }
         }
-        vector<vector<int> > ans2;
-        int sizeofans = ans.size();
+        // remove repeated items
+        return distinct(matchlist);
+    }
+private:
+    
+    vector<vector<int> > distinct(vector<vector<int> > vecs) {
+        vector<vector<int> > result;
+        int sizeofvecs = vecs.size();
+        
         for (int i = 0; i < sizeofans; i++) {
             bool has = false;
-            for (int j = 0; j < ans2.size(); j++) {
-                if (isEqual(ans[i], ans2[j])) {
+            for (int j = 0; j < result.size(); j++) {
+                if (equal(vecs[i], result[j])) {
                     has = true;
                     break;
                 }
             }
             if (!has) {
-                ans2.push_back(ans[i]);
+                result.push_back(vecs[i]);
             }
         }
-        return ans2;
+        return result;
     }
-private:
-    bool isEqual(vector<int>& a, vector<int>& b) {
+    
+    // check if two vector is equal
+    bool equal(vector<int>& a, vector<int>& b) {
         for (int i = 0; i < 4; i++) {
             if (a[i] != b[i]) return false;
         }
--- Algorithms/cpp/AddandSearchWord-Datastructuredesign.cpp
+++ Algorithms/cpp/AddandSearchWord-Datastructuredesign.cpp
@@ -1,10 +1,48 @@
+// Source : https://leetcode.com/problems/add-and-search-word-data-structure-design/
+// Author : Ke Li
+// Date   : 2016-03-12
+
+/***************************************************************************************
+ *
+ * Design a data structure that supports the following two operations:
+ *
+ * void addWord(word)
+ * bool search(word)
+ *
+ * search(word) can search a literal word or a regular expression string containing
+ * only letters a-z or .. A . means it can represent any one letter.
+ *
+ * For example:
+ *
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
+ *
+ * Note:
+ * You may assume that all words are consist of lowercase letters a-z.
+ *
+ * click to show hint.
+ *
+ * You should be familiar with how a Trie works. If not, please work on this problem:
+ * Implement Trie (Prefix Tree) first.
+ ***************************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+// max index
+const int TRIE_SIZE = 26;
 
 struct TrieNode {
-    TrieNode* next[26];
+    TrieNode* next[TRIE_SIZE];
     bool isLeaf;
-
+    
     TrieNode() {
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < TRIE_SIZE; i++) {
             next[i] = NULL;
         }
         isLeaf = false;
@@ -14,62 +52,60 @@ struct TrieNode {
 class WordDictionary {
 public:
     WordDictionary() {
-        tn = new TrieNode();
+        root = new TrieNode();
     }
-
-    int idx(char c) {
-        return (c - 'a');
-    }
-
+    
     // Adds a word into the data structure.
     void addWord(string word) {
-        TrieNode* t = tn;
-        int len = word.length();
-        for (int i = 0; i < len; i++) {
+        TrieNode* t = root;
+        for (int i = 0; i < word.length(); i++) {
             int c = idx(word[i]);
-            if (t->next[c] == NULL) {
+            if (!t->next[c]) {
                 t->next[c] = new TrieNode();
             }
             t = t->next[c];
         }
         t->isLeaf = true;
     }
-
+    
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
     bool search(string word) {
-        if (word.length() == 0) return false;
-        return searchSuffix(word, 0, tn);
+        return word.length() > 0 && find(word, 0, root);
     }
-
-    bool contains(TrieNode* t, char c) {
-        return (c == '.' || t->next[idx(c)] != NULL);
+    
+private:
+    TrieNode* root; // store all words using trie
+    
+    bool contains(TrieNode* node, char c) {
+        return (c == '.' || node->next[idx(c)]);
     }
-
-    bool searchSuffix(const string& w, int st, TrieNode* t) {
-        if (t == NULL) return false;
-        int len = w.length();
-        if (st == len) {
-            return t->isLeaf;
+    
+    // calculate index of a char in trie
+    int idx(char c) {
+        return (c - 'a');
+    }
+    
+    // find a word in trie tree
+    bool find(const string& word, int pos, TrieNode* node) {
+        if (!node) return false;
+        if (pos == word.length()) {
+            return node->isLeaf;
         }
-        if (w[st] == '.') {
-            for (int i = 0; i < 26; i++) {
-                if (searchSuffix(w, st + 1, t->next[i])) {
+        if (word[pos] == '.') {
+            for (int i = 0; i < TRIE_SIZE; i++) {
+                if (find(word, pos + 1, node->next[i])) {
                     return true;
                 }
             }
             return false;
-        } else {
-            return searchSuffix(w, st + 1, t->next[idx(w[st])]);
         }
+        return find(word, pos + 1, node->next[idx(word[pos])]);
     }
-
-private:
-    TrieNode* tn; // store all words using trie
 };
-
 
 // Your WordDictionary object will be instantiated and called as such:
 // WordDictionary wordDictionary;
 // wordDictionary.addWord("word");
 // wordDictionary.search("pattern");
+
