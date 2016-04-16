f41a10bc53e3deec76cfbf833c32357e9bb3c93e
Ke  Li <kel@splunk.com>
2016-04-16 19:27:31 +0800

    Refactor

--- Algorithms/python/026-RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.py
+++ Algorithms/python/026-RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        if len(nums) == 0:
+        if not nums:
             return 0
         i = 0
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
+        for item in nums:
+            if item != nums[i]:
                 i += 1
-                nums[i] = nums[j]
+                nums[i] = item
         return i + 1
